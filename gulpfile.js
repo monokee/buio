b3ae9596eda97a777e43c77f8343ab1714777ceb
monokee <jonathan@patchflyer.com>
2020-04-12 17:21:49 +0200

    major refactor. - added utility classes for flexbox - removed unused components

---
+++ gulpfile.js
@@ -0,0 +1,30 @@
+const {task, src, dest, series} = require('gulp');
+const sass = require('gulp-sass');
+const rename = require('gulp-rename');
+const autoprefixer = require('gulp-autoprefixer');
+const cleanCSS = require('gulp-clean-css');
+
+exports.build = series(
+
+  function buildCSS() {
+    return src('scss/**/*.scss')
+      .pipe(sass().on('error', sass.logError))
+      .pipe(autoprefixer('last 2 versions'))
+      .pipe(rename('buio.css'))
+      .pipe(dest('css'));
+  },
+
+  function minifyCSS() {
+    return src('css/buio.css')
+      .pipe(cleanCSS({
+        level: {
+          1: {
+            specialComments: 'none',
+          }
+        }
+      }))
+      .pipe(rename('buio.min.css'))
+      .pipe(dest('css'));
+  }
+
+);
